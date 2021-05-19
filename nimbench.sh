efab86f704a3427d42ab046d8924b2b8f1e4652f
alexander <alexander@saporo.io>
2021-05-19 20:47:05 +0300

    linux script

--- nimbench.sh
+++ nimbench.sh
@@ -2,79 +2,61 @@
 
 DIR=benchnim
 
-#rm -rf $DIR
-[[ -d $DIR ]] && { echo Remove $DIR folder before run; exit 1; }
-
-#rm -rf $HOME/.cache/nim/*_[dr]
-for D in koch_d nim_r nimble_r nimgrep_r nimpretty_r nimsuggest_r testament_r; do
-  C="$HOME/.cache/nim/$D"
-  [[ -d $C ]] && { echo Cache found $C , remove before run; exit 1; }
-done
-
-export TIMEFORMAT='%3R seconds'
+rm -rf $HOME/.cache/nim/{koch_d,testament_r} 
+exit 1
 
 collectinfo() {
-  [[ "$(uname)" == "Linux" ]] && {
-    printf 'Processor:    '
-      awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//'
-      printf 'CPU cores:    '
-      awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo
-      printf 'Frequency:    '
-      awk -F: ' /cpu MHz/ {freq=$2} END {print freq " MHz"}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//'
-      printf 'RAM:          '
-      free -h | awk 'NR==2 {print $2}'
+  OS=$(uname)
+  [[ $OS == "Linux" ]] && {
+    awk -F: '/model name/ {name=$2} END {print "CPU:" name}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//'
+    awk -F: '/model name/ {core++} END {print "Cores:" core}' /proc/cpuinfo
+    awk -F: ' /cpu MHz/ {freq=$2} END {print "Freq:" freq " MHz"}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//'
+    free -h | awk 'NR==2 {print "RAM:" $2}'
+    echo "OS: $OS"
+    echo "Disk:" `lsblk -n -d -o VENDOR,MODEL | head -n1`
   } || {
-    printf 'Processor:    '
-    sysctl -n hw.model
-    printf 'CPU cores:    '
-    sysctl -n hw.ncpu
-    printf 'Frequency:    '
-    grep -Eo -- '[0-9.]+-MHz' /var/run/dmesg.boot | tr -- '-' ' ' | sort -u
-    printf 'RAM:          '
-    sysctl -n hw.physmem | B_to_MiB
+    echo 'CPU: ' `sysctl -n hw.model`
+    echo 'Cores: ' `sysctl -n hw.ncpu`
+    echo 'Freq: ' `sysctl -n hw.cpufrequency`
+    echo 'RAM: ' `sysctl -n hw.physmem`
+    echo "Disk:" `lsblk -n -d -o VENDOR,MODEL | head -n1`
+    echo "OS: $OS"
   }
 }
 
 
-prep() {
-  cp -r NimOrig Nim && \
+bench_cmd() {
   (
-    cd Nim && \
+    echo Benching $@
+    cd Nim && /usr/bin/time -f "%e seconds: $*" -a -o ../time.log $@
+  )
+}
+
+mkdir -p $DIR
+(
+  cd $DIR && \
+  rm -f time.log && \
+  collectinfo >> time.log
+  [[ ! -d NimCloned ]] && (
+    git clone https://github.com/nim-lang/Nim NimCloned && \
+    cd NimCloned && \
     git reset --hard fac5bae7b7d87aeec48c7252029c2852ee157ac9 && \
     source ci/funs.sh && \
-    nimDefineVars
+    nimDefineVars && \
     echo_run git clone -q --depth 1 $nim_csourcesUrl "$nim_csourcesDir" && \
     echo_run git -C "$nim_csourcesDir" checkout $nim_csourcesHash
   )
-}
 
-bench_cmd() {
-  local PREF=$1
-  shift
-  (
-    echo Benching $@
-    cd Nim && /usr/bin/time -f "%e seconds $PREF: $*" -a -o ../time.log $@
-  )
-}
+  #[[ -d Nim ]] && rm -rf Nim
+  #cp -r NimCloned Nim && \
+  #bench_cmd ./build_all.sh && \
+  #bench_cmd ./koch temp -d:release && \
 
-mkdir $DIR
-(
-  cd $DIR && \
-  collectinfo >> time.log
-  git clone https://github.com/nim-lang/Nim NimOrig && \
-  prep && \
+  cat time.log && \
+  TITLE=`cat time.log | grep ^CPU: | tr '\n ' '!+' | sed 's/!/%0A/g'`
+  BODY=`cat time.log | tr '\n ' '!+' | sed 's/!/%0A/g'` && \
+  URL="https://github.com/inv2004/build_nim_benchmarks/issues/new?title=$TITLE&labels=stats&body=%60%60%60%0A$BODY%60%60%60"
 
-  bench_cmd 1st ./build_all.sh &&
-  bench_cmd 1st ./koch temp -d:release && \
-
-  rm -rf Nim && prep && \
-
-  bench_cmd 2nd ./build_all.sh &&
-  bench_cmd 2nd ./koch temp -d:release && \
-
-  cat time.log
-  BODY=`cat time.log | tr '\n ' '!+' | sed 's/!/%0A/g'`
-  URL="https://github.com/inv2004/build_nim_benchmarks/issues/new?title=Bench&labels=stats&body=%60%60%60%0A$BODY%60%60%60"
   [[ -x $BROWSER ]] && exec "$BROWSER" "$URL" || \
   command -v wslview && exec wslview $URL || \
   command -v open && open "$URL" || \
