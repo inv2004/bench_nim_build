b2577dd8d46e357e0c30265d6df59a6e86e52449
inv2004 <inv2004@gmail.com>
2021-05-20 13:43:44 +0300

    linux compiles

--- nimbench.sh
+++ nimbench.sh
@@ -8,9 +8,9 @@ collectinfo() {
   OS=$(uname)
   [[ $OS == "Linux" ]] && {
     awk -F: '/model name/ {name=$2} END {print "CPU:" name}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//'
-    awk -F: '/model name/ {core++} END {print "Cores:" core}' /proc/cpuinfo
+    awk -F: '/model name/ {core++} END {print "Cores: " core}' /proc/cpuinfo
     awk -F: ' /cpu MHz/ {freq=$2} END {print "Freq:" freq " MHz"}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//'
-    free -h | awk 'NR==2 {print "RAM:" $2}'
+    free -h | awk 'NR==2 {print "RAM: " $2}'
     echo "OS: $OS"
     echo "Disk:" `lsblk -n -d -o VENDOR,MODEL | head -n1`
   } || {
@@ -23,19 +23,33 @@ collectinfo() {
   }
 }
 
-
 bench_cmd() {
   (
     echo Benching $@
     cd Nim && /usr/bin/time -f "%e seconds: $*" -a -o ../time.log $@
-  )
+  ) || { echo Error during build; exit 1; }
+}
+
+complete() {
+  cat >complete.nim <<'EEE'
+import browsers, uri, strformat, strutils, sequtils
+let lines = toSeq(lines("time.log")).deduplicate()
+let title = lines.filterIt(it.startsWith("CPU:")).join().encodeUrl()
+let body = ("```\n" & lines.join("\n") & "\n```").encodeUrl()
+let url = fmt"https://github.com/inv2004/build_nim_benchmarks/issues/new?title={title}&labels=stats&body={body}"
+echo "Please open in your browser the following link if browser is not open:"
+echo url
+openDefaultBrowser(url)
+EEE
+
+./Nim/bin/nim c -r complete.nim
+
 }
 
 mkdir -p $DIR
 (
   cd $DIR && \
-  rm -f time.log && \
-  collectinfo >> time.log
+  collectinfo > time.log
   [[ ! -d NimCloned ]] && (
     git clone https://github.com/nim-lang/Nim NimCloned && \
     cd NimCloned && \
@@ -49,16 +63,9 @@ mkdir -p $DIR
   #[[ -d Nim ]] && rm -rf Nim
   #cp -r NimCloned Nim && \
   #bench_cmd ./build_all.sh && \
-  #bench_cmd ./koch temp -d:release && \
+  #bench_cmd ./koch temp -d:release
 
   cat time.log && \
-  TITLE=`cat time.log | grep ^CPU: | tr '\n ' '!+' | sed 's/!/%0A/g'`
-  BODY=`cat time.log | tr '\n ' '!+' | sed 's/!/%0A/g'` && \
-  URL="https://github.com/inv2004/build_nim_benchmarks/issues/new?title=$TITLE&labels=stats&body=%60%60%60%0A$BODY%60%60%60"
-
-  [[ -x $BROWSER ]] && exec "$BROWSER" "$URL" || \
-  command -v wslview && exec wslview $URL || \
-  command -v open && open "$URL" || \
-  echo "open the link in browser to commit results: $URL"
+  complete
 )
 
